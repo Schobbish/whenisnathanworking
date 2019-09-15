22457419bdf9aa8fd28e076af9b4b7e947a33c7d
Nathan Adam <nathaniellukasadam@gmail.com>
2019-09-14 18:40:48 -0700

    add things

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
         for (var calendar of config.cals) {
             // right now there's only going to be one calendar though
             $.getJSON(`cal/${calendar}.json`).done(function(cal) {
-                for (var event of cal.VCALENDAR.VEVENT) {
-                    $('#cal').append(`<div class="shift" id="event.UID"></div>`);
+                for (var event of cal.VCALENDAR[0].VEVENT) {
+                    $('#cal').append(`<div class="shift" id="${event.UID}"></div>`);
+                    $(event.UID).append(`<p class="job">${event.SUMMARY}</p>`);
+
+                    const startTime = new Date(event.DTSTART);
+                    $(event.UID).append(`<p class="start">${startTime.toDateString} ${startTime.toLocaleTimeString}</p>`);
+
+                    const endTime = new Date(event.DTEND);
+                    $(event.UID).append(`<p class="end">${endTime.toDateString} ${endTime.toLocaleTimeString}</p>`);
                 }
 
             });
