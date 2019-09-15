cb636ba0447b34aaa405efe8fca102b99d0eaa8b
Nathan Adam <nathaniellukasadam@gmail.com>
2019-09-14 18:47:28 -0700

    fix typo and UID issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
     $.getJSON('config.json').done(function(config) {
         for (var calendar of config.cals) {
             // right now there's only going to be one calendar though
-            $.getJSON(`cal/${calendar}.json`).done(function(cal) {
+            $.getJSON(`cals/${calendar}.json`).done(function(cal) {
                 for (var event of cal.VCALENDAR[0].VEVENT) {
                     $('#cal').append(`<div class="shift" id="${event.UID}"></div>`);
-                    $(event.UID).append(`<p class="job">${event.SUMMARY}</p>`);
+                    $(`#${event.UID}`).append(`<p class="job">${event.SUMMARY}</p>`);
 
                     const startTime = new Date(event.DTSTART);
-                    $(event.UID).append(`<p class="start">${startTime.toDateString} ${startTime.toLocaleTimeString}</p>`);
+                    $(`#${event.UID}`).append(`<p class="start">${startTime.toDateString} ${startTime.toLocaleTimeString}</p>`);
 
                     const endTime = new Date(event.DTEND);
-                    $(event.UID).append(`<p class="end">${endTime.toDateString} ${endTime.toLocaleTimeString}</p>`);
+                    $(`#${event.UID}`).append(`<p class="end">${endTime.toDateString} ${endTime.toLocaleTimeString}</p>`);
                 }
 
             });
