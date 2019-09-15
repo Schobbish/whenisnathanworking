bace1b992b1f9df5b11282f6edbd0d2d8b1143c0
Nathan Adam <nathaniellukasadam@gmail.com>
2019-09-14 22:26:01 -0700

    rewrite

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,36 @@
-function correctDateFmt(d) {
-    return new Date(`${d.slice(0,4)}-${d.slice(4,6)}-${d.slice(6,11)}:${d.slice(11,13)}:${d.slice(13)}`);
+function generateCalendar(shifts) {
+    for (const uid in shifts) {
+        if (uid === 'timestamp') {
+            break;
+        }
+        $('#cal').append(`<div class="shift" id="${uid}">
+    ${(new Date(shifts[uid].startDate)).toDateString()} ${shifts[uid].job}
+</div>`);
+    }
+    // set timestamp
+    $('#timestamp').text(shifts.timestamp);
 }
 
 $(document).ready(function() {
+    let public = false;
+    // get config file for list of calendars
     $.getJSON('config.json').done(function(config) {
-        for (var calendar of config.cals) {
-            // right now there's only going to be one calendar though
-            $.getJSON(`cals/${calendar}.json`).done(function(cal) {
-                for (const event of cal.VCALENDAR[0].VEVENT) {
-                    const uid = event.UID.split('@')[0];
-
-                    // check if uid does not exist first
-                    if ($(`#${uid}`).length === 0) {
-                        $('#cal').append(`<div class="shift" id="${uid}"></div>`);
-                        $(`#${uid}`).append(`<p class="start">${correctDateFmt(event.DTSTART).toDateString()}</p>`);
-                        $(`#${uid}`).append(`<p class="job">${event.SUMMARY}</p>`);
-
-                        /*
-                        // not used because times are already in the summary
-                        const startTime = correctDateFmt(event.DTSTART);
-                        $(`#${uid}`).append(`<p class="start">${startTime.toDateString()} ${startTime.toLocaleTimeString()}</p>`);
-
-                        const endTime = correctDateFmt(event.DTEND);
-                        $(`#${uid}`).append(`<p class="end">${endTime.toDateString()} ${endTime.toLocaleTimeString()}</p>`);
-                        */
-                    }
-                }
-            }); // TODO: add fail function
+        for (const cal of config.cals) {
+            // get each calendar's json
+            // those in cals/ are for private use so check those first
+            // those in cals_pub/ are for public use
+            $.getJSON(`cals/${cal}.json`).done(function(shifts) {
+                generateCalendar(shifts);
+            }).fail(function() {
+                // since we fall back on the public files
+                console.log('Ignore that 404 error it\'s expected');
+                public = true;
+                $.getJSON(`cals_pub/${cal}.json`).done(function(shifts) {
+                    generateCalendar(shifts);
+                });
+            });
         }
-    }); // TODO: add fail function
+    }).fail(function() {
+        $('#cal').text('Could not get config.json');
+    });
 });
